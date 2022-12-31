353515aae2210b65aa3c5b09068ed278ec89d70c
David Kinyanjui <dkinyanjui144@gmail.com>
2022-12-31 13:25:27 +0300

    modifued

--- src/Content.js
+++ src/Content.js
@@ -1,14 +1,50 @@
+import { useState } from "react";
+import { FaTrashAlt } from "react-icons/fa";
+
 const Content = () =>{
-    const handleNameChange = ()=>{
-        const names = ['John', 'Paul', 'George', 'Ringo'];
-        const int = Math.floor(Math.random() * 4);
-        return names[int];
+    
+    const [item, setItem] = useState([
+        {
+            id:1,
+            checked: false,
+            item: "One Half Bottle of Cola"
+        },
+        {
+            id: 2,
+            checked: false,
+            item: "Cabagges"
+        },
+        {
+            id:3,
+            checked: false,
+            item: "Potatoes"
+        }
+    ]);
+
+    const handleCheck = (id) =>{
+        setItem(item.map((item) =>{
+            if(item.id === id){
+                item.checked = !item.checked
+            }
+            return item
+        }))
     }
+   
     return(
         <main>
-        <p>
-           Hello {handleNameChange()}!
-        </p></main>
+            <ul>
+                {item.map((item)=> (
+                    <li className="item" key = {item.id}>
+                        <input type="checkbox" onChange={() => handleCheck(item.id)} checked={item.checked}/>
+                        
+                    <label>
+                        {item.item}
+                    </label>
+                    <FaTrashAlt role={"button"} className="trash" tabIndex= "0"/>
+                    </li>
+                ))}
+            </ul>
+        </main>
     )
 }
 export default Content;
